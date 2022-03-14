a790a79cc6ce474036004137a03b9ee326e56ce4
Resty <grande.resty@gmail.com>
2022-03-14 08:28:18 +0800

    Add docker file and store modulle

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { StoreController } from './store/store.controller';
+import { StoreModule } from './store/store.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [MongooseModule.forRoot('mongodb://localhost/Lazhopee')],
+  controllers: [AppController, StoreController],
   providers: [AppService],
 })
 export class AppModule {}
